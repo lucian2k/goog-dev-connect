54adea7a9c6a89d8cd372a26b21eb510075c3690
Lucian Nicolescu <lucian2k@gmail.com>
2013-11-23 19:06:05 +0200

    Checkout API

--- daspi/daspi/apps/api/views.py
+++ daspi/daspi/apps/api/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 
@@ -24,7 +26,7 @@ def dologin(request):
 
 @json_response
 def location(request):
-    return Place.objects.get(pk=settings.THIS_PLACE_PK).to_json()
+    return _get_location().to_json()
 
 @json_response
 def checkin(request):
@@ -33,13 +35,47 @@ def checkin(request):
 
     try: token_obj = UserTokens.objects.get(value=token)
     except: token_obj = None
-
     if not token_obj: return {'error': 'Invalid token', 'success': False}
 
+    place_obj   = _get_location()
+
+    # make sure the user is not checked in here
+    checked_in = _is_checked_in(token_obj.user, place_obj)
+    if checked_in:
+        return {'error': 'Seems the user is already checked in here', 'success': False}
+
     # find the user and register the checkin
-    place_obj   = Place.objects.get(pk=settings.THIS_PLACE_PK)
     checkin_obj = Checkin(user=token_obj.user,
                             place=place_obj)
     checkin_obj.save()
 
     return {'success': True}
+
+@json_response
+def checkout(request):
+    token = request.GET.get('token', None)
+    # token = request.POST.get('token', None)
+
+    try: token_obj = UserTokens.objects.get(value=token)
+    except: token_obj = None
+    if not token_obj: return {'error': 'Invalid token', 'success': False}
+
+    place_obj   = _get_location()
+
+    checked_in = _is_checked_in(token_obj.user, place_obj)
+    if not checked_in:
+        return {'error': 'Seems ;that you are not checked in, you must checkin before attempting to checkout', 'success': False}
+
+    checkin_obj = Checkin.objects.filter(user=token_obj.user,place=place_obj).order_by('-pk')[0]
+    checkin_obj.endtime = datetime.datetime.now()
+    checkin_obj.save()
+
+    return {'success': True}
+
+
+def _get_location():
+    return Place.objects.get(pk=settings.THIS_PLACE_PK)
+
+def _is_checked_in(user, place):
+    return len(Checkin.objects.filter(endtime=None, user=user, place=place)\
+                    .extra(where=["(strftime('%s', 'now') - starttime)/60/60 < 12"])) > 0
