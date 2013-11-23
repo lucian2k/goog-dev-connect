9294c8bee61d18cb7673a9c87c28eb27132f0252
Lucian Nicolescu <lucian2k@gmail.com>
2013-11-23 14:49:12 +0200

    Boilerplate for views

--- daspi/daspi/apps/checkins/views.py
+++ daspi/daspi/apps/checkins/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    """Login and/or eventual redirect"""
+
+    return
+
+
+def reports(request):
+    """The reporting module - see where your bastard is"""
+
+    return
