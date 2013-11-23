814629b6c7f15dad6111a1129ab98541e2cb2621
Lucian Nicolescu <lucian2k@gmail.com>
2013-11-23 14:01:43 +0200

    Added first models

--- daspi/daspi/apps/checkins/models.py
+++ daspi/daspi/apps/checkins/models.py
@@ -10,8 +10,22 @@ USER_TYPES = (
 class ApplePiUser(models.Model):
     user = models.OneToOneField(User)
     role = models.PositiveSmallIntegerField(choices=USER_TYPES)
+    checkins_count = models.PositiveIntegerField(defaul=0)
 
-# Define place types (school, parc, etc)
+# Define place types (school, parc, etc) - defined for each instance in
+# the app's settings
+PLACE_TYPE_CHOICES = (
+    ('1', 'School'),
+    ('2', 'Parc'),
+    ('3', 'Gym')
+)
+class Place(models.Model):
+    place_type = models.PositiveSmallIntegerField(choices=PLACE_TYPE_CHOICES)
+    # Add support for geo fields (spatialite and all)
 
 # Define the checkin model
-
+class Checkin(models.Model):
+    user = models.ForeignKey(User)
+    starttime = models.DateTimeField(auto_now_add=True)
+    endtime = models.DateTimeField(null=True)
+    # add maybe mood or other stuff here
