ea12a9a787d83239b8aa63ba20b87107f23577f7
Lucian Nicolescu <lucian2k@gmail.com>
2013-11-23 13:01:15 +0200

    Extended user model

--- daspi/daspi/apps/checkins/models.py
+++ daspi/daspi/apps/checkins/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Extend the user model
+USER_TYPES = (
+    ('1', 'Checkin only'),
+    ('2', 'Reports only'),
+    ('3', 'All')
+)
+class ApplePiUser(models.Model):
+    user = models.OneToOneField(User)
+    role = models.PositiveSmallIntegerField(choices=USER_TYPES)
+
+# Define place types (school, parc, etc)
+
+# Define the checkin model
+
